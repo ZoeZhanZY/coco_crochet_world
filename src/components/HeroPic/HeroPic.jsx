e70433128d1515a1b5ec81fcb4bc9c1d60dc1db4
Zoe Zhan <zoezhan@Zoes-MacBook.local>
2021-04-15 15:37:42 +1000

    update

---
+++ src/components/HeroPic/HeroPic.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeroPic = () => {
+  return <div></div>;
+};
+
+export default HeroPic;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Products from "./Products/Products";
+import useStyles from "./styles";
+import { Grid } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
 
 const Section = ({ products, onAddToCart, categories }) => {
   /**
@@ -7,6 +11,9 @@ const Section = ({ products, onAddToCart, categories }) => {
    * @param {string} category - The category associated with a product
    * @returns {Array} - The filtered product list based on a category
    */
+
+  const classes = useStyles();
+
   const getProductList = (category) => {
     const filteredProductList = products.filter((product) => {
       const productCategoryList = product.categories.map(
@@ -24,8 +31,14 @@ const Section = ({ products, onAddToCart, categories }) => {
 
     return (
       <div key={category}>
-        <div>Category - {category}</div>
-        <Products products={productList} onAddToCart={onAddToCart} />
+        <main className={classes.content}>
+          <Grid>
+            <CssBaseline />
+            <CssBaseline />
+            <Typography variant="h5">Category - {category}</Typography>
+            <Products products={productList} onAddToCart={onAddToCart} />
+          </Grid>
+        </main>
       </div>
     );
   });
